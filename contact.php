33fbe1d14a89f9d4f94c3444c1fbf24b42c99de7
“Eric <ebar4426@fullsail.edu>
2016-08-18 17:00:19 -0500

    added contact php
    added the code necessary to contact

---
+++ contact.php
@@ -0,0 +1,34 @@
+<?php
+$field_name = $_POST['name_a'];
+$field_email = $_POST['email_a'];
+$field_date = $_POST['date_a'];
+$field_phone = $_POST['phone_a'];
+$field_message = $_POST['message_a'];
+
+$mail_to = 'ebar4426@gmail.com';
+$subject = ' New task from '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Date: '.$field_date."\n";
+$body_message .= 'Phone Number: '.$field_phone."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. We will contact you shortly.');
+		window.location = 'contact_page.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to ebar4426@gmail.com’);
+		window.location = 'placetask.html';
+	</script>
+<?php
